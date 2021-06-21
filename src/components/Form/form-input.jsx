0f7b61cbd4af1476e0738c71b3cdc9520204b638
manu-xo5 <mohit.matwaya@gmail.com>
2021-06-21 17:25:11 +0530

    add: styles for select element

--- src/components/Form/form-input.jsx
+++ src/components/Form/form-input.jsx
@@ -1,14 +1,40 @@
 import './Form.styles.scss';
 import * as React from 'react';
 
-export default ({ as: As = 'input', label, id, labelClass = '', ...rest }) => (
-  <label
-    className={`Input ${
-      rest.type === 'checkbox' || rest.type === 'radio' ? 'inline' : ''
-    } ${labelClass}`}
-    htmlFor={id}
-  >
-    <span>{label}</span>
-    <As name={label} id={id} {...rest} />
-  </label>
-);
+/**
+ * @typedef FormInputProps
+ * @property {string} as
+ * @property {string} label
+ * @property {string} id
+ * @property {string} [labelClass = '']
+ * @property {boolean} inline,
+ */
+
+/** @type {React.FC.<FormInputProps | HTMLInputElement>} */
+const FormInput = ({
+  as: As = 'input',
+  label,
+  id,
+  labelClass = '',
+  labelStyle,
+  inline,
+  ...rest
+}) => {
+  const shouldInline =
+    inline || rest.type === 'checkbox' || rest.type === 'radio';
+  // works when inline
+  const shouldShowLabel = inline && !label ? false : true;
+
+  return (
+    <label
+      style={labelStyle}
+      className={`Input ${shouldInline ? 'inline' : ''} ${labelClass}`}
+      htmlFor={id}
+    >
+      {shouldShowLabel ? <span>{label}</span> : null}
+      <As name={label} id={id} {...rest} />
+    </label>
+  );
+};
+
+export default FormInput;
